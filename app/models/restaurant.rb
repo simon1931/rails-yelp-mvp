f2e514e30dc751426440ddba42dad914aead76d2
Simon Herout <simon@herout.eu>
2021-08-11 12:23:24 +0200

    Les routes et les methodes manquent

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,7 +3,7 @@ class Restaurant < ApplicationRecord
 
   AUTHORIZED_CATEGORY = %w[chinese italian japanese french belgian].freeze
   validates :name, presence: true
-  validates :adress, presence: true
+  validates :address, presence: true
   validates :category, presence: true
   validates :category, inclusion: { in: AUTHORIZED_CATEGORY }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,6 @@ class Review < ApplicationRecord
   AUTHORIZED_RATING = (0..5).freeze
   validates :content, presence: true
   validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: { in: AUTHORIZED_RATING }
 end
